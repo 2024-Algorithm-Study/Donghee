1c7d88ad88bbeb218d434f6261ad0a5e00aa77d8
ddhi7 <144508379+ddhi7@users.noreply.github.com>
2024-02-27 10:43:41 +0900

    Update 소트인사이드.java

--- 8주차/소트인사이드.java
+++ 8주차/소트인사이드.java
@@ -3,20 +3,21 @@ import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
+        Scanner sc=new Scanner(System.in); 
+        
+        String str=sc.next(); //정렬하려는 수 N 입력받음
+        String[] num=str.split(""); //N을 한 글자씩 배열에 저장
+        int arr[]=new int[num.length]; //배열 num의 길이만큼 정수배열 생성
 
-		String str = sc.next();
-		String[] num = str.split("");
-		int arr[] = new int[num.length];
+        for(int i=0;i<num.length;i++){
+            arr[i]=Integer.parseInt(num[i]); //배열 num의 숫자로된 문자를 정수로 변환
+        }
 
-		for (int i = 0; i < num.length; i++) {
-			arr[i] = Integer.parseInt(num[i]);
-		}
-
-		Arrays.sort(arr);
-
-		for (int i = arr.length - 1; i >= 0; i--) {
-			System.out.print(arr[i]);
-		}
-	}
+        Arrays.sort(arr); //정렬
+        
+        //정렬된 arr 배열을 뒤에서 부터 출력
+        for(int i=arr.length-1;i>=0;i--){
+            System.out.print(arr[i]);
+        }
+    }
 }
